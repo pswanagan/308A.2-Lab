b7eb5721d129b13dbdf9e6b5f880f4036a1b595b
pswanagan <pswanagan@gmail.com>
2024-01-01 20:38:47 -0600

    completed Part three

--- script.js
+++ script.js
@@ -16,14 +16,13 @@ const adventurer = {
             type: "Flea",
             inventory: ["small hat", "sunglasses"]
         }
-        
     },
-    roll (mod = 0) {
+    roll: function(mod = 0) {
         const result = Math.floor(Math.random() * 20) + 1 + mod;
-        console.log(`${this.name} rolled a ${result}.`)
-        }
+        console.log(`${this.name} rolled a ${result}.`);
+    }
+};
 
-    };
 
     // Access Robin's inventory using a combination of dot notation and square bracket syntax
 
@@ -31,9 +30,10 @@ const adventurer = {
 
 // Create a loop that logs each item in Robin's inventory
 
-for (const key in adventurer.inventory) {
-    console.log(adventurer.inventory[key]);
-}
+adventurer.inventory.forEach(item => {
+    console.log(item);
+});
+
 
 // Test Dice Roll method a few times
 
@@ -52,7 +52,7 @@ adventurer.roll();
 //Create basic Character Class
 
 class Character {
-    constructer (name){
+    constructor (name){
         this.name = name;
         this.health = 100;
         this.inventory = [];
@@ -64,9 +64,10 @@ class Character {
 }
 
 // Recreate Robin using Character Class
-
+/*
 const robin = new Character("Robin");
-robin.inventory =["sword", "potion", "artifact"];
+robin.inventory = ["sword", "potion", "artifact"];
+
 robin.companion = new Character("Leo");
 robin.companion.type = "Cat";
 robin.companion.companion = new Character("Frank");
@@ -77,6 +78,86 @@ robin.companion.companion.inventory = ["small hat", "sunglasses"];
 
 robin.roll();
 robin.companion.roll();
-robin.companion.companion.roll();
+robin.companion.companion.roll(); */
 
-/
+/**
+ *  Part Three - Class Features
+ */
+
+class Adventurer extends Character {
+    constructor(name, role) {
+        super(name);
+        this.role = role;
+        this.stamina = 100;
+        this.skills = [];
+        this.inventory.push("bedroll", "50 gold coins");
+    }
+
+    scout() {
+        console.log(`${this.name} is scouting ahead...`);
+        super.roll();
+    }
+
+    equipItem(item) {
+        console.log(`${this.name} is equipping ${item}.`);
+        // Additional logic to equip the item
+    }
+
+    rest() {
+        console.log(`${this.name} is resting.`);
+        this.health = 100;
+        this.stamina = 100;
+        // Additional logic for resting
+    }
+
+    addSkill(skill) {
+        this.skills.push(skill);
+        console.log(`${this.name} has learned ${skill}.`);
+    }
+}
+
+
+class Companion extends Character {
+    constructor(name, type) {
+        super(name);
+        this.type = type;
+        this.loyalty = 100;
+        this.specialAbility = "";
+    }
+
+    follow() {
+        console.log(`${this.name} is now following.`);
+    }
+
+    assist() {
+        console.log(`${this.name} is assisting.`);
+    }
+
+    setSpecialAbility(ability) {
+        this.specialAbility = ability;
+        console.log(`${this.name} has the special ability: ${ability}`);
+    }
+}
+
+const robin = new Adventurer("Robin", "Scout");
+robin.addSkill("Archery");
+
+const leo = new Companion("Leo", "Cat");
+leo.setSpecialAbility("Night Vision");
+
+const frank = new Companion("Frank", "Flea");
+frank.setSpecialAbility("Stealth");
+
+robin.companion = leo;
+leo.companion = frank;
+
+robin.scout();
+robin.rest();
+robin.equipItem("sword");
+
+leo.follow();
+leo.assist();
+
+frank.follow();
+frank.assist();
+
